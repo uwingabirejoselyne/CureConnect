b7bcfdc072657a36b8ae59bc1c20e0a0f57a5bc5
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-11 21:05:54 +0200

    created contact route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import About from './pages/About'
+import Contact from './pages/Contact'
+
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10]'>
@@ -13,6 +15,7 @@ const App = () => {
         <Route path='/doctors/:specialist' element={<Doctors/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </div>
   )
