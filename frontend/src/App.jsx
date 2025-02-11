30cd835cfdd94c04c7b72eda394726eda2183f59
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-11 21:03:41 +0200

    created about route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
+import About from './pages/About'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10]'>
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/doctors' element={<Doctors/>}/>
         <Route path='/doctors/:specialist' element={<Doctors/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </div>
   )
