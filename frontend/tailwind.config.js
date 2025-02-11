156a7fe6b0a71182b1ce7ee1a9cff079d0949b96
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-11 11:08:11 +0200

    tailwind installation

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ensure this path covers your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
