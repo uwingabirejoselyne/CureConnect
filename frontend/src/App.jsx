4122ce4aef57c507443208eaeb47f5ad4ca56be6
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-11 20:58:45 +0200

    created login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
-
+import Login from './pages/Login'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10]'>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:specialist' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   )
