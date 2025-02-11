5a840b0a76cda6a0214dfeb2e17ca9b8b5db63bd
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-11 12:24:10 +0200

    home page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-yellow-500 text-center'>hello</h1>
+    <div className='mx-4 sm:mx-[10]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
