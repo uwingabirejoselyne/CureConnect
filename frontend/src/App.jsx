7d13abfe63cbdf06ecd6e146bb19901ca788cdc6
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-11 21:15:43 +0200

    created  appointment  route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+
 
 const App = () => {
   return (
@@ -20,7 +22,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointments' element={<MyAppointments/>}/>
-
+        <Route path='/appointment' element={<Appointment/>}/>
 
       </Routes>
     </div>
