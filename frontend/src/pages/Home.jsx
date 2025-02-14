b5e693bd76f6c29e2e5a22febf80341eba245843
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-14 10:20:39 +0200

    completed specialist menu section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialistMenu from '../components/SpecialistMenu'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <SpecialistMenu/>
     </div>
   )
 }
