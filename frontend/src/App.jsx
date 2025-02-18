3338dcb1cfc6ccb086f20aa04cf5318e2d760a59
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-18 18:31:22 +0200

    completed footer section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -26,8 +27,8 @@ const App = () => {
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointments' element={<MyAppointments/>}/>
         <Route path='/appointment/:docId' element={<Appointment/>}/>
-
       </Routes>
+      <Footer/>
     </div>
     </Router>
   )
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { assets } from '../assets/assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='md:mx-10'>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40'>
+                <div>
+                    <img className='mb-5 w-40' src={assets.logo} alt="" />
+                    <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, deserunt beatae? Exercitationem, ducimus quis! Quibusdam.</p>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
+                        <li>+250785991851</li>
+                        <li>uwingajoselyne@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
