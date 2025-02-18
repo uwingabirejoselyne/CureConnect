25394d33d033a837569d1a80cebbe0dbd268e850
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-18 11:11:39 +0200

    created context

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialistMenu from '../components/SpecialistMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <SpecialistMenu/>
+      <TopDoctors/>
     </div>
   )
 }
