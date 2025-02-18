2d501a96eb171fc6ea3b656b9f9aee8477070be3
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-02-18 11:17:34 +0200

    completed top doctors component

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -5,6 +5,7 @@ import { AppContext } from '../context/AppContext'
 const TopDoctors = () => {
   const navigate = useNavigate()
   const {doctors} = useContext(AppContext)
+
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
@@ -25,7 +26,7 @@ const TopDoctors = () => {
             ))
         }
       </div>
-      <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+      <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
