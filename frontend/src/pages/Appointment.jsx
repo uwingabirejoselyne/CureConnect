658c4c984241a77442577ef8029121ffe3942716
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-02 16:40:22 +0200

    related doctors section

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -120,6 +121,7 @@ const Appointment = () => {
         </div>
         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
+      <RelatedDoctors docId={docId} speciality ={docInfo.speciality}/>
     </div>
   );
 };
