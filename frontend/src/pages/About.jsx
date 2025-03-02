d6d8e769121107ad88996b2550d611ce63c7e1d6
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-02 20:07:05 +0200

    completed about us section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,7 +16,23 @@ const About = () => {
           <p>Our vision at prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between the patients and healthcare providers, making it easier to access the care you need, When you need it.</p>
         </div>
       </div>
-      
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+        <div className='flex flex-col md:flex-row mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[var(--bg-primary)] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+            <b>Efficiency:</b>
+            <p>Streamlined appointment scheduling that fits into your busy lifestyle. </p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[var(--bg-primary)] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Conviniency:</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[var(--bg-primary)] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalisation:</b>
+          <p>Tailored recommendations and reminders to help you to stay on top of your health.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
