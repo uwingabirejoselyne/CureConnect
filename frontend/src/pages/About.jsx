f6f49e36f44cf176ec314321d7781379e4fae654
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-02 17:15:19 +0200

    created about as section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {assets} from '../assets/assets/assets'
 
 const About = () => {
   return (
     <div>
-      About
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+      <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to prescripto, your trusted partener in managing your healthcare needs Conviniently and Efficiently. At Prescripto we understand the challenges individual face when it comes to scheduling doctors.</p>
+          <p>Prescripto is committed to excellence in healthcare technology. We continously Strive to enhance our platform,Integrating the latest advancement to improve user experience and deliver superior service.whether your'e booking first appointment or managing ongoing care.Prescripto is here to support you every step of the way.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision at prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between the patients and healthcare providers, making it easier to access the care you need, When you need it.</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
