bf456b73bf9169448f388a97dfc443b0ea483df8
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-04 11:00:52 +0200

    completed responsiviness

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -5,6 +5,7 @@ import {AppContext} from '../context/AppContext'
 const Doctors = () => {
   const {speciality} = useParams();
   const [filterDoc,setFilterDoc] = useState([]);
+  const [showfilter, setShowFilter] = useState(false)
  const {doctors} = useContext(AppContext)
  const navigate = useNavigate()
   
@@ -26,7 +27,8 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browse through the doctors specialicity.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`py-1 px-3 border rounded-sm text-sm transition-all sm:hidden ${showfilter ? 'bg-blue-500 text-white':''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showfilter ? 'flex' : 'hidden sm:flex'}`}>
           <p onClick={()=> speciality === 'General physician' ? navigate('/doctors'):navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality ==="General physician"?"bg-indigo-100 text-black":""}`}>General Physician</p>
           <p onClick={()=> speciality === 'Gynecologist' ? navigate('/doctors'):navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality ==="Gynecologist"?"bg-indigo-100 text-black":""}`}>Gnecologist</p>
           <p onClick={()=> speciality === 'Dermatologist' ? navigate('/doctors'):navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality ==="Dermatologist"?"bg-indigo-100 text-black":""}`}>Dermatologist</p>
