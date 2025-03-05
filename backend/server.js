c0e8c3b3cd766c0fab1919e3a99098a930c75df6
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-05 09:56:17 +0200

    starting the server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("Api working")
+})
+
+app.listen(port, ()=>console.log("Server started",port))
