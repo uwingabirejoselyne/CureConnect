69f1756c8dbc753db3ddc484560085edf08f3390
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-05 11:12:25 +0200

    cloudinary configuration

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDb = require('./config/mongodb');
+const cloudinary = require('./config/cloudinary')
 
 
 const app = express()
 const port = process.env.PORT || 4000
 connectDb()
+cloudinary()
 
 app.use(express.json())
 app.use(cors())
