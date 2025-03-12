d8be72481e010833e1c0d637b088faa9d4434cda
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-12 09:43:01 +0200

    vite installation

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const upload = require('../middlewares/multer');
 const { addDoctor, loginAdmin } = require('../controllers/adminController');
+const authAdmin = require('../middlewares/autAdmin');
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor', upload.single('image'), addDoctor);
+adminRouter.post('/add-doctor', authAdmin  ,upload.single('image'), addDoctor);
 adminRouter.post('/login',loginAdmin)
 
 module.exports = adminRouter;
