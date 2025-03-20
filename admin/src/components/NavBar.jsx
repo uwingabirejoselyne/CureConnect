a9e61b62c347ea4236f5741e0070a120f3a00097
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-03-20 10:51:10 +0200

    created side bar for admin dashboard

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
         aToken && localStorage.removeItem('aToken')
     }
   return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg:white'>
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
       <div className='flex items-center gap-2 text-sm'>
         <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
         <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ?'Admin': 'Doctor'}</p>
--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const SideBar = () => {
+  const {aToken} = useContext(AdminContext)
   return (
-    <div>
-      sidebar
+    <div className='min-h-screen bg-white border-r'>
+      {
+        aToken && <ul className='text-[#515151] mt-5'>
+          <NavLink className={({isActive}) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor ${isActive ?'bg-[#f2f3ff] border-r-4 border-primary':''}`} to={'/admin-dashboard'}>
+            <img src={assets.home_icon} alt="" />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink className={({isActive}) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor ${isActive ?'bg-[#f2f3ff] border-r-4 border-primary':''}`} to={'/all-appointnments'}>
+            <img src={assets.appointment_icon} alt="" />
+            <p>Appointnments</p>
+          </NavLink>
+          <NavLink className={({isActive}) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor ${isActive ?'bg-[#f2f3ff] border-r-4 border-primary':''}`} to={'/add-doctor'}>
+            <img src={assets.add_icon} alt="" />
+            <p>Add Doctor</p>
+          </NavLink>
+          <NavLink className={({isActive}) =>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor ${isActive ?'bg-[#f2f3ff] border-r-4 border-primary':''}`} to={'/doctor-list'}>
+            <img src={assets.people_icon} alt="" />
+            <p>Doctor Lists</p>
+          </NavLink>
+        </ul>
+      }
     </div>
   )
 }
