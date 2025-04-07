f14ce89598c28fe0354de357137626dac3672741
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-04-07 21:28:02 +0200

    created change doctor availability method

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,17 @@
+const doctorModel = require("../models/doctorModel");
+
+const changeAvailability = async(req,res)=>{
+    try {
+        const{docId} = req.body
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId,{available:!docData.available})
+        res.json({success:true,message:'Availability changed'})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+        
+    }
+
+}
+module.exports = {changeAvailability}
