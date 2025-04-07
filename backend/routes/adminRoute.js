96e0ebe4078ee9eb506a25ccee0bf23f3d5ddbb4
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-04-07 21:38:27 +0200

    created change doctor availability route

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,11 +2,14 @@ const express = require('express');
 const upload = require('../middlewares/multer');
 const { addDoctor, loginAdmin, allDoctors } = require('../controllers/adminController');
 const authAdmin = require('../middlewares/autAdmin');
+const { changeAvailability } = require('../controllers/doctorController');
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add-doctor', authAdmin  ,upload.single('image'), addDoctor);
 adminRouter.post('/login',loginAdmin)
 adminRouter.get('/all-doctors',authAdmin,allDoctors)
+adminRouter.post('/change-availability',authAdmin,changeAvailability)
+
 
 module.exports = adminRouter;
