f35f340ca7134646f04225aff474d917549ec054
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-04-09 15:23:28 +0200

    implemented login user method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,4 +40,24 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+const loginUser = async(req,res) =>{
+    try {
+        const {email,password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            res.json({success:false,message:"User does not exist"})
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(isMatch){
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false,message:"Invalid credentials"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+module.exports = { registerUser,loginUser };
